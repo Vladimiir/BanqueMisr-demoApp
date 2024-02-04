7dfc7004da8374e4b62670e69fa66cd77ed1466b
Vladimir <lamaxur@gmail.com>
2024-02-04 17:56:56 +0400

    init

---
+++ BanqueMisr-demoApp/BanqueMisr-demoApp/BanqueMisr_demoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanqueMisr_demoAppApp.swift
+//  BanqueMisr-demoApp
+//
+//  Created by Vladimir Stasenko on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BanqueMisr_demoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BanqueMisr-demoApp/BanqueMisr-demoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BanqueMisr-demoApp
+//
+//  Created by Vladimir Stasenko on 04.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle(Text("Currency!"))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
